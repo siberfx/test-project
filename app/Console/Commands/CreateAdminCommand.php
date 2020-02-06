63c85e6421178342f03e1cd20a161f5222d465eb
Selim <=>
2020-02-06 15:05:26 +0300

    creating admin by command line

---
+++ app/Console/Commands/CreateAdminCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class CreateAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creating an admin user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = new User();
+        $user->password = bcrypt('demo');
+        $user->email = 'admin@demo.com';
+        $user->name = 'Admin User';
+        $user->save();
+    }
+}
